1af92e7748269c4a9319382a5251d672b74cc1f5
ThiagoRiquelmo <riquelmemq@gmail.com>
2021-07-19 22:09:41 -0300

    Copiei a funcao quick sort do codigo em c

--- Lista.h
+++ Lista.h
@@ -43,6 +43,39 @@ public:
         cout<<"\n";
     }
 
+    int partition(int *vetor, int left, int right)
+{
+	int i, j, aux, pivo = vetor[right];
+
+    i = (left - 1);
+
+    for (j = left; j <= right- 1; j++)
+    {
+        if (vetor[j] < pivo)
+        {
+            i++;
+            aux = vetor[i];
+			vetor[i] = vetor[j];
+			vetor[j] = aux;
+        }
+    }
+	aux = vetor[i+1];
+	vetor[i+1] = vetor[right];
+	vetor[right] = aux;
+    return i + 1;
+}
+
+void quick_sort(int *vetor, int left, int right)
+{
+	int p;
+	 if (left < right)
+    {
+        p = partition(vetor, left, right);
+        quick_sort(vetor, left, p - 1);
+        quick_sort(vetor, p + 1, right);
+    }
+}
+
 };
 
 #endif
